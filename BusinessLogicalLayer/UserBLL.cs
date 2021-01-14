d6d3e649070eb32f6f27a9acc8c699eedc786770
arthureduardomolinari@gmail.com <77454923+bernardowerner@users.noreply.github.com>
2021-01-14 14:00:17 -0300

    UP

--- BusinessLogicalLayer/UserBLL.cs
+++ BusinessLogicalLayer/UserBLL.cs
@@ -1,9 +1,45 @@
-﻿using Entities;
+﻿using BusinessLogicalLayer.Interfaces;
+using Common;
+using Entities;
 using System;
 
 namespace BusinessLogicalLayer
 {
-    public class UserBLL
+    public class UserBLL : IUserService
     {
+        public Response Insert(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response Update(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response Delete(int id)
+        {
+            /*
+            User user = new User();
+            user.ID = id;
+
+            using (DietDB db = new DietDB())
+            {
+                db.Entry(user).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+            }
+            return ResponseMessage.CreateSuccessResponse();
+            */
+        }
+
+        public QueryResponse<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SingleResponse<User> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
